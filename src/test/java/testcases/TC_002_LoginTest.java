bf0667933cd1d16d0ea16dfea5e40de503facb5a
GitPrac12345 <gitgitdemoprac@gmail.com>
2024-09-30 22:24:42 +0530

    c1

---
+++ src/test/java/testcases/TC_002_LoginTest.java
@@ -0,0 +1,35 @@
+package testcases;
+
+public class TC_002_LoginTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("UserName");
+		System.out.println("Password");
+		System.out.println("Login");
+
+		// Updated Login
+
+		System.out.println("UserName1");
+		System.out.println("Password1");
+		System.out.println("Login1");
+
+		// Updated Login
+
+		System.out.println("UserName2");
+		System.out.println("Password2");
+		System.out.println("Login2");
+
+		// Updated Login
+
+		System.out.println("UserName3");
+		System.out.println("Password3");
+		System.out.println("Login3");
+		// Updated Login
+void updatedLogin(){
+		-----updated Login
+		System.out.println("updated Login");
+	}
+	}
+
+}
---
+++ src/test/java/testcases/TC_003_PaymentTest.java
@@ -0,0 +1,18 @@
+package testcases;
+
+public class TC_002_LoginTest {
+
+	public static void main(String[] args) {
+
+		System.Out.Println("Payment")
+
+}
+public static void main(String[] args) {
+
+		System.Out.Println("Payment8")
+
+}
+change1
+change2
+change3
+change4
